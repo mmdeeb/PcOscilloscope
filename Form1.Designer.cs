7c497725d11b2117a3eca12e7fa19f6aa28abe5f
Mohammed Yousef <mohammedmohammeddeeb@gmail.com>
2022-12-12 16:55:09 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,299 @@
+﻿namespace WindowsFormsApp3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.StartButton = new System.Windows.Forms.Button();
+            this.Up = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.Down = new System.Windows.Forms.Button();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.BaudRate = new System.Windows.Forms.ComboBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.TimeBox = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            this.chart1.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.chart1.BorderlineColor = System.Drawing.Color.WhiteSmoke;
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(5, 12);
+            this.chart1.Name = "chart1";
+            this.chart1.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.None;
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(652, 327);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            this.chart1.Click += new System.EventHandler(this.chart1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.comboBox1.Font = new System.Drawing.Font("Tahoma", 14F);
+            this.comboBox1.ForeColor = System.Drawing.SystemColors.Window;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(787, 44);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(104, 31);
+            this.comboBox1.TabIndex = 3;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.label1.Font = new System.Drawing.Font("Tahoma", 14F, System.Drawing.FontStyle.Bold);
+            this.label1.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.label1.Location = new System.Drawing.Point(675, 47);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 23);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "COM:";
+            // 
+            // StartButton
+            // 
+            this.StartButton.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.StartButton.Font = new System.Drawing.Font("Tahoma", 14F, System.Drawing.FontStyle.Bold);
+            this.StartButton.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.StartButton.Location = new System.Drawing.Point(62, 141);
+            this.StartButton.Name = "StartButton";
+            this.StartButton.Size = new System.Drawing.Size(120, 34);
+            this.StartButton.TabIndex = 1;
+            this.StartButton.Text = "Start";
+            this.StartButton.UseVisualStyleBackColor = false;
+            this.StartButton.Click += new System.EventHandler(this.Start_Click);
+            // 
+            // Up
+            // 
+            this.Up.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.Up.Font = new System.Drawing.Font("Tahoma", 14F, System.Drawing.FontStyle.Bold);
+            this.Up.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.Up.Location = new System.Drawing.Point(13, 51);
+            this.Up.Name = "Up";
+            this.Up.Size = new System.Drawing.Size(94, 31);
+            this.Up.TabIndex = 6;
+            this.Up.Text = "UP";
+            this.Up.UseVisualStyleBackColor = false;
+            this.Up.Click += new System.EventHandler(this.Up_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Tahoma", 14F, System.Drawing.FontStyle.Bold);
+            this.label3.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.label3.Location = new System.Drawing.Point(70, 14);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(95, 23);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Position:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Tahoma", 14F, System.Drawing.FontStyle.Bold);
+            this.label4.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.label4.Location = new System.Drawing.Point(84, 107);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(64, 23);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Time:";
+            // 
+            // Down
+            // 
+            this.Down.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.Down.Font = new System.Drawing.Font("Tahoma", 14F, System.Drawing.FontStyle.Bold);
+            this.Down.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.Down.Location = new System.Drawing.Point(141, 51);
+            this.Down.Name = "Down";
+            this.Down.Size = new System.Drawing.Size(89, 31);
+            this.Down.TabIndex = 7;
+            this.Down.Text = "DOWN";
+            this.Down.UseVisualStyleBackColor = false;
+            this.Down.Click += new System.EventHandler(this.Down_Click);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.trackBar1.Location = new System.Drawing.Point(11, 133);
+            this.trackBar1.Maximum = 1000;
+            this.trackBar1.Minimum = 1;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(217, 45);
+            this.trackBar1.TabIndex = 8;
+            this.trackBar1.Value = 1;
+            this.trackBar1.Scroll += new System.EventHandler(this.Time_Scroll);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.label2.Font = new System.Drawing.Font("Tahoma", 14F, System.Drawing.FontStyle.Bold);
+            this.label2.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.label2.Location = new System.Drawing.Point(675, 90);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(109, 23);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "BaudRate:";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.HighlightText;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(77, 341);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(468, 74);
+            this.dataGridView1.TabIndex = 10;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel1.Controls.Add(this.StartButton);
+            this.panel1.Controls.Add(this.BaudRate);
+            this.panel1.Location = new System.Drawing.Point(661, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(245, 190);
+            this.panel1.TabIndex = 11;
+            // 
+            // BaudRate
+            // 
+            this.BaudRate.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.BaudRate.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.BaudRate.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BaudRate.Font = new System.Drawing.Font("Tahoma", 14F);
+            this.BaudRate.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.BaudRate.FormattingEnabled = true;
+            this.BaudRate.Location = new System.Drawing.Point(126, 70);
+            this.BaudRate.Name = "BaudRate";
+            this.BaudRate.Size = new System.Drawing.Size(104, 31);
+            this.BaudRate.TabIndex = 3;
+            // 
+            // panel2
+            // 
+            this.panel2.AccessibleDescription = "";
+            this.panel2.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel2.Controls.Add(this.Up);
+            this.panel2.Controls.Add(this.trackBar1);
+            this.panel2.Controls.Add(this.Down);
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Controls.Add(this.label4);
+            this.panel2.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.panel2.Location = new System.Drawing.Point(661, 225);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(245, 190);
+            this.panel2.TabIndex = 11;
+            // 
+            // TimeBox
+            // 
+            this.TimeBox.Location = new System.Drawing.Point(737, 421);
+            this.TimeBox.Name = "TimeBox";
+            this.TimeBox.Size = new System.Drawing.Size(100, 20);
+            this.TimeBox.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.ClientSize = new System.Drawing.Size(928, 447);
+            this.Controls.Add(this.TimeBox);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panel2);
+            this.Name = "Form1";
+            this.Text = "OscilloscopePc";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox BaudRate;
+        private System.Windows.Forms.Button StartButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button Up;
+        private System.Windows.Forms.Button Down;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.TextBox TimeBox;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Text;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            dataGridView1.CellMouseUp += DataGridView_CellMouseUp;
+            dataGridView1.ColumnCount = 1;
+            dataGridView1.ColumnHeadersVisible = false;
+            dataGridView1.RowHeadersVisible = false;
+            dataGridView1.Columns[0].Width =1000;
+            string[] ports = SerialPort.GetPortNames();
+
+            string[] items =new string[] { "9600","14400","38400","57600","76800","115200,200000"};
+            foreach (var item in items)
+            {
+                BaudRate.Items.Add(item);
+            }
+            foreach (var port in ports)
+            {
+                comboBox1.Items.Add(port);
+               
+            }
+            
+        }
+        private int rowIndex = 0;
+        private void DataGridView_CellMouseUp(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right)
+            {
+                this.dataGridView1.Rows[e.RowIndex].Selected = true;
+                this.rowIndex = e.RowIndex;
+                this.dataGridView1.CurrentCell = this.dataGridView1.Rows[e.RowIndex].Cells[0];
+                this.contextMenuStrip1.Show(this.dataGridView1, e.Location);
+                contextMenuStrip1.Show(System.Windows.Forms.Cursor.Position);
+            }
+        }
+        double currentXMax = 10;
+        double currentXMin = 1;
+        double currentYMax = 6;
+        double currentYMin = -6;
+        void setScale()
+        {
+            if (currentXMax > currentXMin && currentYMax > currentYMin)
+            {
+             
+                chart1.ChartAreas[0].AxisY.Maximum = currentYMax;
+                chart1.ChartAreas[0].AxisY.Minimum = currentYMin;
+            }
+        }
+        List<String> Ex = new List<String>();
+        double T = 25;
+        double X = 1;
+        private void Start_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+            port = new SerialPort(comboBox1.Text,Convert.ToInt32(BaudRate.Text),Parity.None,8,StopBits.One);
+            if(port.IsOpen)
+                port.Close();
+            port.Open();
+                try
+                {
+                    Thread masterThread;
+                    masterThread = new Thread(Runit);
+                    masterThread.Start();
+
+                }
+                catch (Exception ex)
+                {
+                    Ex.Add(ex.Message);
+                    dataGridView1.Rows.Add("Error read from serial: "+ex.Message);
+                    dataGridView1.ClearSelection();
+                    dataGridView1.Rows[Ex.Count - 1].Cells[0].Style.BackColor = Color.DarkGoldenrod;
+                    dataGridView1.Rows[Ex.Count - 1].Cells[0].Style.ForeColor = Color.White;
+
+                }
+               /* try
+                {
+                    Thread myThread;
+                    myThread = new Thread(setTime);
+                    myThread.Start();
+
+                }
+                catch (Exception ex)
+                {
+                    Ex.Add(ex.Message);
+                    dataGridView1.Rows.Add("Error Thread Time: " + ex.Message);
+                    dataGridView1.ClearSelection();
+                    dataGridView1.Rows[Ex.Count - 1].Cells[0].Style.BackColor = Color.DarkGoldenrod;
+                    dataGridView1.Rows[Ex.Count - 1].Cells[0].Style.ForeColor = Color.White;
+
+                }*/
+
+            }
+            catch (Exception ex)
+            {
+                Ex.Add(ex.Message);
+                dataGridView1.Rows.Add(ex.Message);
+                dataGridView1.ClearSelection();
+                dataGridView1.Rows[Ex.Count - 1].Cells[0].Style.BackColor = Color.DarkRed;
+                dataGridView1.Rows[Ex.Count - 1].Cells[0].Style.ForeColor = Color.White;
+            }
+           void Runit()
+            {
+                
+                while (port.IsOpen)
+                {
+                    try
+                    {
+                        
+                        byte re;
+                        re=Convert.ToByte(port.ReadByte());
+                        getValue = Convert.ToByte(port.ReadByte());
+                        int getValueI = Convert.ToInt32(getValue);
+                        
+                        chart1.Invoke((MethodInvoker)(() => chart1.Series["Series1"].Points.AddXY(T, Convert.ToDouble(getValueI) / 51.2)));
+                        //Console.WriteLine( "hello");
+                        T +=25;
+                        currentXMax = T;
+                        currentXMin =currentXMax- 10;
+                        //chart1.ChartAreas[0].AxisY.Minimum = currentXMin;
+
+
+
+
+                    }
+                    catch (Exception ex)
+                    {
+                        Ex.Add(ex.Message);
+                        dataGridView1.Rows.Add(ex.Message);
+                        dataGridView1.ClearSelection();
+                        dataGridView1.Rows[Ex.Count - 1].Cells[0].Style.BackColor = Color.DarkGreen;
+                        dataGridView1.Rows[Ex.Count - 1].Cells[0].Style.ForeColor = Color.White;
+                    }
+                    
+                }
+
+            }
+
+
+
+        }
+        SerialPort port;
+        Byte getValue;
+        
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void Up_Click(object sender, EventArgs e)
+        {
+            currentYMax +=1;
+            currentYMin +=1;
+            setScale();
+        }
+
+        private void Down_Click(object sender, EventArgs e)
+        {
+            currentYMax -= 1;
+            currentYMin -= 1;
+            setScale();
+
+        }
+         void setTime()
+        {
+            Thread.Sleep(200);
+            chart1.ChartAreas[0].AxisX.Maximum = currentXMax;
+            
+            chart1.ChartAreas[0].AxisX.Minimum  =currentXMin;
+
+
+        }
+        void setTimeT()
+        {
+            X = trackBar1.Value;
+
+        }
+
+        private void Time_Scroll(object sender, EventArgs e) 
+        {
+        setTimeT(); 
+        }
+
+        private void chart1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+       
+    }
+}
